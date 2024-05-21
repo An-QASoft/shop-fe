dba24367db87ed707aac16830e2ec8a3aa4b7017
An <seo.nguyenhoangan@gmail.com>
2024-05-21 15:48:55 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+import axiosInstance from './axiosConfig';
+
+const fetchUsers = async () => {
+  try {
+    const response = await axiosInstance.get('/products/index');
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
+};
+
+fetchUsers();
+</script>
